5a2d8b24394d828a8dca19dea084e2b6165ed612
juanoned <juandiego.jd42@gmail.com>
2024-08-21 19:50:46 -0300

    Start backend Project

---
+++ models/user.js
@@ -0,0 +1,22 @@
+///models/user.js
+const Sequelize = require("sequelize");
+module.exports= (sequelize) => {
+    const User = sequelize.define('User',{
+        id:{
+            type:Sequelize.INTEGER,
+            autoincrement:true,
+            primaryKey:true,
+            allowNull:false
+        },
+        email:{
+            type:Sequelize.STRING,
+            unique:true,
+            allowNull:false
+        },
+        password:{
+            type:Sequelize.STRING,
+            allowNull:false
+        }
+    });
+    return User;
+};
